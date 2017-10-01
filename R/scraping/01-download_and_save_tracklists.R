ff17159cf531c4232b04fffaca5f825691f82daf
Sillas Gonzaga <sillas.gonzaga@gmail.com>
2017-10-01 01:53:59 +0000

    as

--- R/scraping/01-download_and_save_tracklists.R
+++ R/scraping/01-download_and_save_tracklists.R
@@ -5,10 +5,15 @@ source("R/funcoes/_extract_page.R")
 
 pgs <- 1:last.page
 
-nn <- length(pgs)
+# configure bigquery
+#proj <- "hardstyle-181514"
+#dset <- "LSDB"
+#tb_name <- "TB_LSDB_SETS_DATA"
+# token
+#token <- readRDS(".httr-oauth")
 #set_access_cred(token)
 #set_service_token(token)
-set_oauth2.0_cred(token$`161c2c22532513a6d62fcb025cef84b8`$app)
+#set_oauth2.0_cred(token$`161c2c22532513a6d62fcb025cef84b8`$app)
 
 nn <- length(pgs)
 for (i in 1:nn){
@@ -16,7 +21,6 @@ for (i in 1:nn){
   # while condition to make it keep running until it returns a data frame
   df <- NULL
   attempt <- 1
-  max.attempts <- 20
   max.attempts <- 5
   while( is.null(df) && attempt <= max.attempts) {
     attempt <- attempt + 1
@@ -25,16 +29,16 @@ for (i in 1:nn){
       silent = TRUE
     )
   } 
-  
-  # convert columns types
-  df$set.date %<>% as.Date()
-  df$upload.date %<>% ymd_hms()
-  df$length %<>% str_sub(1, 8)
-  df$length %<>% hms()
-  # replace dots in colnames
-  names(df) <- str_replace_all(names(df), "\\.", "_")
-  # upload it to bigquery
-  insert_upload_job(proj, dset, tb_name, values = df)
+  # 
+  # # convert columns types
+  # df$set.date %<>% as.Date()
+  # df$upload.date %<>% ymd_hms()
+  # df$length %<>% str_sub(1, 8)
+  # df$length %<>% hms()
+  # # replace dots in colnames
+  # names(df) <- str_replace_all(names(df), "\\.", "_")
+  # # upload it to bigquery
+  # insert_upload_job(proj, dset, tb_name, values = df)
 }
 
 
